afbde6c95151203e5494ff7a2df4a2e63779f4fe
Twetler <tuliowetler@gmail.com>
2021-09-19 12:06:51 -0300

    Adding param_loader() and experiment_loader()

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -2,6 +2,12 @@ import yaml
 
 
 def param_loader() -> list:
+    with open('src/config/parameters.yaml') as file:
+        parameters_list = yaml.load(file, Loader=yaml.FullLoader)
+        return parameters_list
+
+
+def experiment_loader() -> list:
     with open('src/config/experiments.yaml') as file:
         experiment_list = yaml.load(file, Loader=yaml.FullLoader)
         return experiment_list
