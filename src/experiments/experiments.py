516cd0f1f2a6bfe7aa7d10464327b78a4a0c3a0f
Twetler <tuliowetler@gmail.com>
2021-09-19 12:06:29 -0300

    Adding __str__ to Experiment Model

--- src/experiments/experiments.py
+++ src/experiments/experiments.py
@@ -1,5 +1,8 @@
 class Experiment:
-    def __init__(self, conversion_arr):
+    def __init__(self, conversion_arr, experiment_name):
+        self.name = experiment_name
+        self.time = [5, 15, 30, 45, 60, 75, 90, 105, 120]
+        self.volume = 0.35
         if len(conversion_arr) > 9:
             # Then it has additional information
             self.has_additional_info = True
@@ -11,4 +14,24 @@ class Experiment:
         else:
             self.has_additional_info = False
             self.conversion = conversion_arr
-            self.time = [5, 15, 30, 45, 60, 75, 90, 105, 120]
+
+    def __str__(self):
+        if self.has_additional_info:
+            return print(
+                'Experiment Name: ', self.name,
+                '\n Volume: ', self.volume,
+                '\n Time Array: ', self.time,
+                '\n Conversions: ', self.conversion,
+                '\n Temperature: ', self.temperature,
+                '\n H Ions Per Mass: ', self.h_ions_per_mass,
+                '\n Catalyst Concentration: ', self.catalyst_conc,
+                '\n Ion Exchange Capacity: ', self.ion_exchange_cap
+                )
+        else:
+            return print(
+                'Experiment Name: ', self.name,
+                '\n Time Array: ', self.time,
+                '\n Conversions: ', self.conversion
+                )
+
+
